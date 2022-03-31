4d24ec9c2bcbdbac6fa724791e9d848eebad50bf
Varun Gungi <54449583+meenank@users.noreply.github.com>
2022-03-31 10:42:46 +0530

    Delete 3_Implementation/documentation/html directory

--- 3_Implementation/documentation/html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['documentation_5fmain_0',['documentation_main',['../md__d___certs__m1__ipl_data_viewer_3__implementation__inc_documentation_main.html',1,'']]]
-];
--- 3_Implementation/documentation/html/search/pages_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['inc_0',['Inc',['../md__d___certs__m1__ipl_data_viewer_3__implementation__inc_readme.html',1,'']]]
-];
